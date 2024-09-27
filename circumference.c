ebb949e860c7a99bd32dba3ee9b822d322b947d6
amiteshk987 <160836253+amiteshk987@users.noreply.github.com>
2024-09-27 15:47:46 +0530

    Add files via upload

---
+++ circumference.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float r,c;
+	float pie=3.14;
+	printf("enter the radius",r);
+	scanf("%f",&r);
+	c=2*pie*r;
+	printf("circumference of circle is equal to: %f\n",c);
+	
+	
+}
